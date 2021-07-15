234c292a91ebe0bba7b50ae433cc55dc1be5da17
ahe <ahe@devvm4633.ftw0.facebook.com>
2021-07-14 17:45:49 -0700

    [3/N] Nnapi Delegate: OSS Tests
    Summary:
    Added a unit test for the Nnapi delegate's preprocess() function. The
    function was previously tested locally, but now a basic test is
    added for OSS.
    
    See https://github.com/pytorch/pytorch/pull/61499 for preprocess
    implementation. See D29647123 for local testing.
    
    TODO:
    Add more comprehensive tests.
    Add tests for model execution, after the Nnapi delegate's initialization
    and execution is implemented T91991928.
    
    CMakeLists.txt:
    Added a library for the Nnapi delegate
    - Explicit linking of torch_python is necessary for the Nnapi delegate's
    use of pybind
    
    test_backends.py:
    Added a test for lowering to Nnapi
    - Based off https://github.com/pytorch/pytorch/blob/master/test/test_nnapi.py
    - Only differences are the loading of the nnapi backend library and the
    need to change dtype from float64 to float32
    
    Test Plan:
    Running `python test/test_jit.py TestBackendsWithCompiler -v` succeeds.
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    T91991864
    Tags:
    
    ghstack-source-id: ae07afcd22a6b6f119b41e9230c8501aa3c65dac
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61594

--- test/jit/test_backends.py
+++ test/jit/test_backends.py
@@ -3,6 +3,7 @@ import io
 import os
 import sys
 import unittest
+import ctypes
 
 import torch
 import torch._C
@@ -17,6 +18,7 @@ from torch.testing._internal.common_utils import (
     IS_WINDOWS,
     TEST_WITH_ROCM,
     skipIfRocm,
+    TEST_WITH_ASAN
 )
 # Make the helper files in test/ importable
 pytorch_test_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -685,3 +687,71 @@ class TestBackendsWithCompiler(JitTestCase):
     @skipIfRocm
     def test_errors(self):
         self.error_module_compiler_test.test_errors()
+
+"""
+Unit Tests for Nnapi backend with delegate
+"""
+class NnapiBackendPReLUTest(JitTestCase):
+    """
+    Test lowering a simple PRelU module to Nnapi backend.
+    TODO: After Nnapi delegate is finished T91991928,
+    add tests for running the module (currently only lowers the model)
+    """
+    def setUp(self):
+        super().setUp()
+        # Load nnapi delegate library
+        torch_root = Path(__file__).resolve().parent.parent.parent
+        p = torch_root / 'build' / 'lib' / 'libnnapi_backend.so'
+        torch.ops.load_library(str(p))
+
+        # Nnapi set up
+        # Avoid saturation in fbgemm
+        torch.backends.quantized.engine = 'qnnpack'
+
+        libneuralnetworks_path = os.environ.get("LIBNEURALNETWORKS_PATH")
+        if libneuralnetworks_path:
+            ctypes.cdll.LoadLibrary(libneuralnetworks_path)
+            print("Will attempt to run NNAPI models.")
+            self.can_run_nnapi = True
+        else:
+            self.can_run_nnapi = False
+
+    def test_execution(self):
+        # Save default dtype
+        module = torch.nn.PReLU()
+        default_dtype = module.weight.dtype
+        # Change dtype to float32 (since float 64 is not supported)
+        torch.set_default_dtype(torch.float32)
+        module = torch.nn.PReLU()
+        args = torch.tensor([[1.0, -1.0, 2.0, -2.0]]).unsqueeze(-1).unsqueeze(-1)
+
+        # Trace and lower PreLu module
+        traced = torch.jit.trace(module, args)
+        compile_spec = {"forward": {"inputs": args}}
+        nnapi_model = torch._C._jit_to_backend("nnapi", traced, compile_spec)
+
+        # Change dtype back to the default
+        torch.set_default_dtype(default_dtype)
+
+# This is needed for IS_WINDOWS or IS_MACOS to skip the tests.
+@unittest.skipIf(TEST_WITH_ROCM or IS_SANDCASTLE or IS_WINDOWS or IS_MACOS or IS_FBCODE,
+                 "Non-portable load_library call used in test")
+# ASAN is currently causing an error. It is still unclear how to resolve this.
+# See https://fb.workplace.com/groups/1144215345733672/permalink/2010505639104634/
+@unittest.skipIf(TEST_WITH_ASAN, "Cannot test with ASAN")
+class TestNnapiBackend(JitTestCase):
+    """
+    This class wraps all Nnapi test classes
+    TODO: Add more comprehensive Nnapi tests (currently only a basic one)
+    """
+    def __init__(self, name):
+        super().__init__(name)
+        self.prelu_test = NnapiBackendPReLUTest(name)
+
+    def setUp(self):
+        if not TEST_WITH_ROCM:
+            self.prelu_test.setUp()
+
+    @skipIfRocm
+    def test_execution(self):
+        self.prelu_test.test_execution()
